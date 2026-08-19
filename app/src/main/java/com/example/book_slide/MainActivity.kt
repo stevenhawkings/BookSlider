88655d4379ca5d1405c3c55fffb9af8b6c56e93b
SilverSickBot <SilverSickBot>
2026-08-18 18:47:17 -0600

    Archivos para iniciar con el proyecto

---
+++ app/src/main/java/com/example/book_slide/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.book_slide
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tvSaludo = findViewById<TextView>(R.id.tvSaludo)
+        val btnActualizar = findViewById<Button>(R.id.btnActualizar)
+        btnActualizar.setOnClickListener {
+            val hora = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            val saludo = when (hora) {
+                in 0..11 -> "¡Buenos días!, Listo para leer"
+                in 12..18 -> "¡Buenas tardes!, Listo para leer?"
+                else -> "¡Buenas noches!, Listo para leer?"
+            }
+            tvSaludo.text = saludo
+        }
+    }
+}
