9c8a5a7bd7940bab957fa4d9874d7464c741d65c
SilversickBot <Silversick64@gmail.com>
2026-08-19 18:55:38 -0600

    .

--- app/src/main/java/com/example/book_slide/Pantalla2.kt
+++ app/src/main/java/com/example/book_slide/Pantalla2.kt
@@ -10,7 +10,7 @@ class Pantalla2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pantalla2)
+        setContentView(R.layout.fragment_menu_books)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/book_slide/Pantalla3.kt
+++ app/src/main/java/com/example/book_slide/Pantalla3.kt
@@ -10,7 +10,7 @@ class Pantalla3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pantalla3)
+        setContentView(R.layout.fragment_book)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
